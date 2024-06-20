8852a2af95559d3e00d64d6d37c0d11b14ddc692
AbishekFranklin <abishek6996@gmail.com>
2024-06-20 07:13:25 -0400

    Add Docker support

--- service/__init__.py
+++ service/__init__.py
@@ -1,10 +1,3 @@
-"""
-Package: service
-Package for the application models and service routes
-This module creates and configures the Flask app and sets up the logging
-and SQL database
-"""
-import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
@@ -18,10 +11,7 @@ talisman = Talisman(app)
 CORS(app)
 
 # Import the routes After the Flask app is created
-# pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
-
-# pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
 # Set up logging for production
@@ -33,9 +23,8 @@ app.logger.info(70 * "*")
 
 try:
     models.init_db(app)  # make our database tables
-except Exception as error:  # pylint: disable=broad-except
+except Exception as error:
     app.logger.critical("%s: Cannot continue", error)
-    # gunicorn requires exit code 4 to stop spawning workers when they die
     sys.exit(4)
 
 app.logger.info("Service initialized!")
